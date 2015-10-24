657d26e49dd8b7ba2ee1c71eefd12a8af2c527de
Edward Gomez <edward@gomez.me.uk>
2015-10-24 13:31:41 -0700

    Editing the licence

--- image_ali.py
+++ image_ali.py
@@ -1,3 +1,21 @@
+'''
+ColourImager - Python pipeline to combine 3 FITS files into a single colour JPEG/PNG
+Copyright (C) 2015 Edward Gomez, LCOGT
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
 from astropy.io import fits
 from astroscrappy import detect_cosmics
 import PIL.Image as pli
